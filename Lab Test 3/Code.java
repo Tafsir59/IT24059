4dd0737740630b6853e05505cf0414a7121e5b4b
Tafsir59 <it24059@mbstu.ac.bd>
2026-01-27 19:42:25 +0600

    Update Code.java

--- Lab Test 3/Code.java
+++ Lab Test 3/Code.java
@@ -1,9 +1,5 @@
 import java.util.Scanner;
-
-// I renamed this class to 'FactorionHelper' to avoid conflicts with your other file
 class FactorionHelper {
-
-    // Method to calculate factorial of a digit
     public int factorial(int n) {
         int fact = 1;
         for (int i = 1; i <= n; i++) {
@@ -11,8 +7,6 @@ class FactorionHelper {
         }
         return fact;
     }
-
-    // Method to check if a number is a Factorion
     public boolean isFactorion(int num) {
         int originalNum = num;
         int sum = 0;
@@ -26,12 +20,9 @@ class FactorionHelper {
 
         return sum == originalNum;
     }
-
-    // Method to find and print factorions in a range
     public void findFactorionsInRange(int start, int end) {
         int count = 0;
         System.out.println("--- Finding Factorion Numbers ---");
-
         for (int i = start; i <= end; i++) {
             if (isFactorion(i)) {
                 System.out.println("Found Factorion: " + i);
@@ -46,18 +37,12 @@ class FactorionHelper {
 public class LabTest3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Updated to use the new class name 'FactorionHelper'
         FactorionHelper utils = new FactorionHelper();
-
         System.out.print("Enter Start of Range: ");
         int start = scanner.nextInt();
-
         System.out.print("Enter End of Range: ");
         int end = scanner.nextInt();
-
         utils.findFactorionsInRange(start, end);
-
         scanner.close();
     }
 }
